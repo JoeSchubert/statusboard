c93ace229ff465053d31863928ce3311edb8dda9
Joe Schubert <joseph.schubert@gmail.com>
2019-12-09 18:06:56 -0500

    Add all rates/ranks up to 06
    Instead of repeatedly having to modify these, just add them all.

--- src/statusboard/Constants.java
+++ src/statusboard/Constants.java
@@ -1,6 +1,9 @@
 package statusboard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,6 +18,8 @@ public class Constants {
     public final String DECK = "Deck";
     public final String SUPPORT = "Support";
     public final String TDY = "TDY";
+    private final String[] rates = {"AET", "AMT", "AST", "BM", "CS", "DC", "DV",  "EM", "ET", "GM", "HS", "IT", "IS", "ME", "MK", "MST", "MU", "PA", "OS", "SK", "YN"};
+    private final String[] specialties = {"AVI", "BNDM", "BOSN", "COMM", "ELC", "F&S", "INF", "MAT", "MED", "PERS", "PSS", "PYA", "WEPS"};
 
     public static HashMap<String, Integer> departmentMap = new HashMap<String, Integer>();
 
@@ -49,85 +54,117 @@ public class Constants {
     }
 
     public String[] getPayGrades() {
-        String[] pg = {"", "O6", "O5", "O4", "O3", "O2", "O1", "CADET", "W4", "W3", "W2", "E8", "E7", "E6", "E5", "E4", "E3", "E2"};
+        String[] pg = {"", "O6", "O5", "O4", "O3", "O2", "O1", "CADET", "W4", "W3", "W2", "E9", "E8", "E7", "E6", "E5", "E4", "E3", "E2"};
         return pg;
     }
 
-    public String[] getRanks(String payGrade) {
-        String[] str = new String[]{};
+    public String[]  getRanks(String payGrade) {
+        ArrayList <String>  str = new ArrayList<>();
         switch (payGrade) {
             default:
                 break;
             case "O6": {
-                str = new String[]{"CAPT"};
+                str.add("CAPT");
                 break;
             }
             case "O5": {
-                str = new String[]{"CDR"};
+                str.add("CDR");
                 break;
             }
             case "O4": {
-                str = new String[]{"LCDR"};
+                str.add("LCDR");
                 break;
             }
             case "O3": {
-                str = new String[]{"LT"};
+                str.add("LT");
                 break;
             }
             case "O2": {
-                str = new String[]{"LTJG"};
+                str.add("LTJG");
                 break;
             }
             case "O1": {
-                str = new String[]{"ENS"};
+                str.add("ENS");
                 break;
             }
             case "CADET": {
-                str = new String[]{"CDT"};
+                str.add("CDT");
                 break;
             }
             case "W4": {
-                str = new String[]{"F&S4", "ENG4"};
+                for (String spec : specialties) {
+                    str.add(spec + "4");
+                }
                 break;
             }
             case "W3": {
-                str = new String[]{"F&S3", "ENG3"};
+                for (String spec : specialties) {
+                    str.add(spec + "3");
+                }
                 break;
             }
             case "W2": {
-                str = new String[]{"F&S2", "ENG2"};
+                for (String spec : specialties) {
+                    str.add(spec + "2");
+                }
+                break;
+            }
+            case "E9" : {
+                for (String rate : rates)  {
+                    str.add(rate + "CM");
+                }
                 break;
             }
             case "E8": {
-                str = new String[]{"BMCS", "CSCS", "EMCS", "ETCS", "GMCS", "MKCS", "OSCS"};
+                for (String rate : rates)  {
+                    str.add(rate + "CS");
+                }
                 break;
             }
             case "E7": {
-                str = new String[]{"AETC", "AMTC", "BMC", "CSC", "DCC", "EMC", "ETC", "GMC", "ITC", "MEC", "MKC", "OSC", "SKC", "YNC"};
+                for (String rate : rates) {
+                    str.add(rate + "C");
+                }
                 break;
             }
             case "E6": {
-                str = new String[]{"AET1", "AMT1", "BM1", "CS1", "DC1", "EM1", "ET1", "GM1", "IT1", "IS1", "ME1", "MK1", "OS1", "SK1", "YN1"};
+                for (String rate : rates) {
+                    str.add(rate + "1");
+                }
                 break;
             }
             case "E5": {
-                str = new String[]{"AET2" , "AMT2", "BM2", "CS2", "DC2", "EM2", "ET2", "GM2", "IT2", "IS2", "ME2", "MK2", "OS2", "SK2", "YN2"};
+                for (String rate : rates) {
+                    str.add(rate + "2");
+                }
                 break;
             }
             case "E4": {
-                str = new String[]{"AET3", "AMT3", "BM3", "CS3", "DC3", "EM3", "ET3", "GM3", "IT3", "IS3", "ME3", "MK3", "OS3", "SK3", "YN3"};
+                for (String rate : rates) {
+                    str.add(rate + "3");
+                }
                 break;
             }
             case "E3": {
-                str = new String[]{"SN", "FN", "SNBM", "SNCS", "FNDC", "FNEM", "SNET", "FNGM", "SNIT", "SNIS", "SNME", "FNMK", "SNOS", "SNSK", "SNYN"};
+                str.add("SN");
+                str.add("FN");
+                for (String rate : rates) {
+                    str.add(getPrefix(rate, "E3") + rate);
+                }
                 break;
             }
             case "E2": {
-                str = new String[]{"SA", "FA"};
+                str.add("SA");
+                str.add("FA");
+                for (String rate : rates) {
+                    str.add(getPrefix(rate, "E2") + rate);
+                }
                 break;
             }
         }
-        return str;
+        String[] retString = new String[str.size()];
+        retString = str.toArray(retString);
+        return retString;
     }
 
     public String[] getDepartments() {
@@ -136,5 +173,23 @@ public class Constants {
 
     public String getDatabaseName() {
         return "jdbc:sqlite:StatusBoard.db";
+    }
+    
+    private String getPrefix(String rating, String paygrade) {
+        List<String> deck  = Arrays.asList("AET","AST", "BM", "CS", "DV", "ET", "HS", "IT", "IS", "ME", "MST",  "OS",  "PA",  "SK", "YN");
+        if (deck.contains(rating)) {
+            if (paygrade.equals("E3")) {
+                return "SN";
+            }  else if (paygrade.equals("E2")) {
+                return "SA";
+            }
+        } else {
+            if (paygrade.equals("E3")) {
+                return "FN";
+            } else if (paygrade.equals("E2")) {
+                return "FA";
+            }   
+        }
+        return "";
     }
 }
